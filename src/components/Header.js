95b2217685bc73d5a5171100757138b165c9e268
Maria Lafutkina <lafutkina.masha@yandex.ru>
2023-08-21 11:07:11 +0300

    feature: to make app

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import back from "../images/icon-back.svg";
+
+function Header() {
+  return (
+    <header>
+      <img src={back} alt="кнопка назад" />
+    </header>
+  );
+}
+
+export default Header;
